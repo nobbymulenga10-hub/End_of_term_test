4d6bacc77e972d6a6d08faad3032e8c8e40d6058
nobby mulenga <nobbymulenga10@gmail.com>
2025-12-26 18:55:32 +0200

    Add files via upload

---
+++ ict113_variables.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    char name[] = "Mulenga Nobby";
+    int age = 25;
+    float gpa = 3.8;
+
+    printf("Name: ");
+      printf("%s\n", name);
+
+    printf("Age: ");
+     printf("%d\n", age);
+
+    printf("GPA: ");
+      printf("%.1f\n", gpa);
+
+    return 0;
+}
